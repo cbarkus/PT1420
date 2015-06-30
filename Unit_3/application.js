ca32e7e271ebe51982fcd0f5bc84e509fdd018ab
cbarkus <cardell.w.barkus@gmail.com>
2015-06-29 20:26:48 -0500

    Take out the dog working

--- Unit_3/application.js
+++ Unit_3/application.js
@@ -1,4 +1,11 @@
 function addToDo() {
 	var toDoList = [];
+	var list
+	list = document.getElementById("todolist");	
+	toDoList[0] = "Take out the dog"
+	
+	list.innerHTML += '<div class="item">' + toDoList[0] + '</div>';
+	
+
 	
 }
