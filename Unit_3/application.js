5e69cf11b759d40120985ed84e0e50c280184f36
cbarkus <cardell.w.barkus@gmail.com>
2015-06-29 20:38:49 -0500

    store list

--- Unit_3/application.js
+++ Unit_3/application.js
@@ -2,7 +2,7 @@ function addToDo() {
 	var toDoList = [];
 	var list
 	list = document.getElementById("todolist");	
-	toDoList[0] = "Take out the dog"
+	toDoList[0] = document.getElementById("todonew").value;
 	
 	list.innerHTML += '<div class="item">' + toDoList[0] + '</div>';
 	
