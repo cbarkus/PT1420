f40e8a9ba20926a9c09fc5aa4b7cedd6cd6cc2b9
cbarkus <cardell.w.barkus@gmail.com>
2015-07-20 19:00:54 -0500

    not sure what happen here

--- Unit_3/js/application.js
+++ Unit_3/js/application.js
@@ -2,26 +2,41 @@
 var toDoList = [];
 
 function loadToDoList() {
-	if(localStorage.todolist) {
-	toDoList = JSON.parse(localStorage.todolist);
+	if(localStorage.toDoList) {
+	toDoList = JSON.parse(localStorage.toDoList);
 	changeDisplay();
 	}
+
 }
 
 function changeDisplay() {
-	var list = document.getElementById("todolist");	//ONLY PLACE WE CHANGE THE SCREEN
+	var list = document.getElementById("todolist");
 	list.innerHTML = "";
 	for(var i=0; i<toDoList.length; i++){
 		list.innerHTML += '<li class="list-group-item">' + toDoList[i] + 
 		'<div class="btn-group-xs pull-right">' +
 		'<button class="btn btn-warning delete" onclick="javascript:removeToDo(' + i +')">' + 
 		'Delete</button>' + 
-		'<button class="btn btn-warinig edit" onclick="javascript:editToDo(' + i +')">' +
+		'<button class="btn btn-default edit" onclick="javascript:editToDo(' + i +')">' +
 		'Edit</button>' +
-		'</div>'
+		'</div>' +
 		'</li>';
-	}	
-	localStorage.todolist = JSON.stringify(toDoList);
+		}	
+		localStorage.toDoList = JSON.stringify(toDoList);
+}
+
+function editToDo(itemToEdit) {
+	var newList = [];
+	for(var i=0;i<toDoList.length;i++) {
+		if(i === itemToEdit) {
+			var editedToDo = prompt (toDoList[i]);
+			newList.push(editedToDo);
+		}else{
+			newList.push(toDoList[i]);
+		}
+	}
+	toDoList = newList;
+	changeDisplay();
 }
 
 function removeToDo(itemToRemove) {
@@ -29,17 +44,13 @@ function removeToDo(itemToRemove) {
 	for(var i=0;i<toDoList.length;i++) {
 		if( i !== itemToRemove) {
 			newList.push(toDoList[i]);
+		}
 		
 	}
-	toDoList = newList;
-	changeDisplay();
+
 }
 
-function editToDo() {
-	for(var i=0;i,toDoList.length;i++) {
-		if( i !==)
-	}
-}
+
 function addToDo() {
 	var tmpItem;
 	var newToDo = document.getElementById("todonew");	tmpItem = newToDo.value;
@@ -49,12 +60,11 @@ function addToDo() {
 		return;
 		
 	}
-}
 
 	toDoList.push(tmpItem);
-	//Call our display function
-	changeDisplay();
 	
+	changeDisplay();
+
 	newToDo.value = "";
 	newToDo.select();
 }
