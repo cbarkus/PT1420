7545fb5548de6caddb9b1b684658b5320fb053d5
cbarkus <cardell.w.barkus@gmail.com>
2015-07-20 19:51:01 -0500

    new button

--- Quiz_4/application.js
+++ Quiz_4/application.js
@@ -1,9 +1,19 @@
-function HelloWorld () {
+function helloWorld () {
 	for(var i = 100; i >= 0; i=i-1) {
 		if (i === 0) {
-			document.write(finalmessage);
+			document.write();
 			} else {
 			document.write("<p>Hello World</p>");
+		}
+	}
+}
+
+function goodByeWorld () {
+	for(var i = 100; i >= 0; i=i-1) {
+		if (i === 0) {
+			document.write();
+			} else {
+			document.write("<p>GoodBy World</p>");
 		}
 	}
 }
---
+++ Quiz_4/notes.js
@@ -0,0 +1,5 @@
+function printHello () {
+	var turkmanistan = document.getElementById("output")
+	for(var i = 0; i<100; i++)
+		turkmanistan.innerHTML += "<div>Hello World</div>";
+}
