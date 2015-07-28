36cf800b280e4b02a677ffde1837927c1846494b
cbarkus <cardell.w.barkus@gmail.com>
2015-07-27 20:57:43 -0500

    Moving Square

--- canvas/application.js
+++ canvas/application.js
@@ -1,8 +1,21 @@
 var canvas = document.getElementById("board");
 var drawing = canvas.getContext("2d");
-var pos = {x: 10, y: 10};
+//THis is the ball
+var pos = {x: 10, y: 10, vx: 15, vy: 15, sx: 10, sy: 10};
+var interval;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth - 20;
+canvas.height = window.innerHeight - 20;
 
-drawing.fillRect(pos.x, pos.y, 50, 50)
+drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
+
+function loop() {
+	drawing.clearRect(0,0,canvas.width, canvas.height);
+	//Adjust our postion by velocity of x
+	pos.x = pos.x + pos.vx;
+	//Adjust our postion by velocity of y
+	pos.y = pos.y + pos.vy;
+	drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
+}
+
+interval = setInterval(loop, 50);
