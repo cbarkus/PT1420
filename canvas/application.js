2b4b2327e70db368ac506e207930571c24971370
cbarkus <cardell.w.barkus@gmail.com>
2015-07-27 21:59:56 -0500

    bounce of Pad

--- canvas/application.js
+++ canvas/application.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight - 10;
 //This is the ball
 var pos = {x: 10, y: 10, vx: 10, vy: 10, sx: 10, sy: 10};
 //This is the paddle
-var pad = {x: (canvas.width / 2), y: (canvas.height / 2), sx: 20, sy: 80};
+var pad = {x: (canvas.width / 2), y: (canvas.height / 2), sx: 20, sy: 1ß80};
 var interval;
 
 
@@ -15,10 +15,18 @@ drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
 
 function loop() {
 	drawing.clearRect(0,0,canvas.width, canvas.height);
+	
 	//Adjust our postion by velocity of x and y
 	pos.x = pos.x + pos.vx;
 	pos.y = pos.y + pos.vy;
 	
+	if(pos.x > pad.x && pos.y > pad.y) {
+		if(pos.x < (pad.x + pad.sx) && pos.y < (pad.y + pad.sy)) {
+			pos.vx = pos.vx * -1;
+		}
+	}
+
+	//change direction if it hits a wall
 	if(pos.x >= canvas.width - 20) {pos.vx = pos.vx * -1; }
 	if(pos.y >= canvas.height - 20) {pos.vy = pos.vy * -1; }
 	if(pos.x <= 0) {pos.vx = pos.vx * -1; }
