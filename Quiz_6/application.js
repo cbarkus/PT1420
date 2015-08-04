5a591dc80990e3ac7ad7f36d8cf9178190d7a085
cbarkus <cardell.w.barkus@gmail.com>
2015-08-04 15:18:40 -0500

    Made the Quiz function without a button and added a Message.

--- Quiz_6/application.js
+++ Quiz_6/application.js
@@ -3,7 +3,7 @@ function printText () {
 	content.innerHTML = "";
 	for(var i = 0; i<1001; i++) {
 		if(i % 9 == 0 || i % 17 == 0) {
-		content.innerHTML += "<div>" + i + "</div>";	
+		content.innerHTML += "<div>" + i + "</div>";
 		}
 		
 	}
