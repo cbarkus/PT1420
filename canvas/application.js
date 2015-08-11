565139189c135ab889e2990286be6162b24ed469
cbarkus <cardell.w.barkus@gmail.com>
2015-08-10 19:43:07 -0500

    working081075

--- canvas/application.js
+++ canvas/application.js
@@ -2,57 +2,76 @@ var canvas = document.getElementById("board");
 var drawing = canvas.getContext("2d");
 canvas.width = window.innerWidth - 10;
 canvas.height = window.innerHeight - 10;
+var playerLeft;
+var playerRight;
+var scores;
 
 //This is the ball
 var pos = {x: 10, y: 10, vx: 10, vy: 10, sx: 10, sy: 10};
 //This is the paddle
-var pad = {x: canvas.width * 0.85, y: (canvas.height / 2), sx: 20, sy: canvas.height / 5.6};
+var padR = {x: canvas.width * 0.85, y: (canvas.height / 2), sx: 20, sy: canvas.height / 5.6};
+var padL = {x: canvas.width * 0.15, y: (canvas.height / 2), sx: 20, sy: canvas.height / 5.6};
 var interval;
 var dist = canvas.height / 5;
 
-
-
 drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
-
 
 function loop() {
 	drawing.clearRect(0,0,canvas.width, canvas.height);
 	
-	//Adjust our postion by velocity of x and y
+//Adjust our postion by velocity of x and y
 	pos.x = pos.x + pos.vx;
 	pos.y = pos.y + pos.vy;
 	
-	if(pos.x > pad.x && pos.y > pad.y) {
-		if(pos.x < (pad.x + pad.sx) && pos.y < (pad.y + pad.sy)) {
+	if(pos.x > padR.x && pos.y > padR.y) {
+		if(pos.x < (padR.x + padR.sx) && pos.y < (padR.y + padR.sy)) {
 			pos.vx = pos.vx * -1;
 		}
 	}
+	if(pos.x > padL.x && pos.y > padL.y) {
+		if(pos.x < (padL.x + padL.sx) && pos.y < (padL.y + padL.sy)) {
+			pos.vx = pos.vx * -1;	
+		}
+	}		
 
-	//change direction if it hits a wall
+//change direction if it hits a wall
 	if(pos.x >= canvas.width - 20) {pos.vx = pos.vx * -1; }
 	if(pos.y >= canvas.height - 20) {pos.vy = pos.vy * -1; }
 	if(pos.x <= 0) {pos.vx = pos.vx * -1; }
 	if(pos.y <= 0) {pos.vy = pos.vy * -1; }
 		
-	drawing.fillRect(pad.x, pad.y, pad.sx, pad.sy);	drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
+	drawing.fillRect(padR.x, padR.y, padR.sx, padR.sy);
+	drawing.fillRect(padL.x, padL.y, padL.sx, padL.sy);	drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
 }
 
 document.onkeydown = function(e) {
 	
 	if(e.keyCode == 38) {
-		if(pad.y <= 0) {
+		if(padR.y <= 0) {
 			return false;
 		}
-			pad.y = pad.y - dist;
+			padR.y = padR.y - dist;
 			return false;
 	} else if (e.keyCode == 40) {
-		if((pad.y + pad.sy) >= canvas.height) {
+		if((padR.y + padR.sy) >= canvas.height) {
 			return false;
 		} 
-		pad.y = pad.y + dist;
+		padR.y = padR.y + dist;
 			return false;
-			
+		}
+		if(e.keyCode == 87) {
+			if(padL.y <= 0) {
+			return false;
+		}
+			padL.y = padL.y - dist;
+			return false;
+	} else if (e.keyCode == 83) {
+		if((padL.y + padL.sy) >= canvas.height) {
+			return false;
+		} 
+			padL.y = padL.y + dist;
+			return false;
 		}
 }
 
-interval = setInterval(loop, 10);
+interval = setInterval(loop, 20);
