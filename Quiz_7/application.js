e379d69df243706db8dfe2e5917b805ec153fd3b
cbarkus <cardell.w.barkus@gmail.com>
2015-08-10 20:02:54 -0500

    updates

--- Quiz_7/application.js
+++ Quiz_7/application.js
@@ -1,8 +1,9 @@
 function printText () {
-	var Input1 = document.getElementById("Input1");	var Input2 = document.getElementById("Input2");	var Input3 = document.getElementById("Input3");	var content = document.getElementById("output");
+	var start = document.getElementById("start");	var end = document.getElementById("end");	
+	var divisible = document.getElementById("divisible");	var content = document.getElementById("output");
 	content.innerHTML = "";
-	for(var i = Input1.value; i < Input2.value; i++) {
-		if(i % Input3.value == 0) {
+	for(var i = start.value; i < end.value; i++) {
+		if(i % divisible.value == 0) {
 		content.innerHTML += "<div>" + i + "</div>";
 		}
 		
